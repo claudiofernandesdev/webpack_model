677a34c0d9fe06ed67a2a81ee21b1c7e677dcfbc
Claudiofernandesdev <claudiofernandes.dev@mail.com>
2023-03-27 10:06:19 -0300

    config project

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// linha 01
+// linha 02
+// linha 03
+console.log('Hellooooooooooooooooooooooooooooo');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
